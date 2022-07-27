fd062eb515f67e3fba1dd3c4d7662cfa2e633dfc
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-07-27 17:39:27 -0300

    added crossorigin to social media controller

--- src/main/java/com/tiagoramirez_portfolio/portfolio/controller/SocialMediaController.java
+++ src/main/java/com/tiagoramirez_portfolio/portfolio/controller/SocialMediaController.java
@@ -3,11 +3,13 @@ package com.tiagoramirez_portfolio.portfolio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tiagoramirez_portfolio.portfolio.model.SocialMedia;
 import com.tiagoramirez_portfolio.portfolio.service.ISocialMediaService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class SocialMediaController {
 
